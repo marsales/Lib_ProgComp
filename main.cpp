d23209d56af6cb49b481fa76ee0356193bfa51c9
Marina Rodas <122946394+marsales@users.noreply.github.com>
2026-07-08 19:34:09 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+#define ll long long
+#define ii pair<int,int>
+#define pll pair<ll, ll>
+
+#define vi vector<int>
+#define vll vector<ll>
+#define forn(i, n) for (int i = 0; i < n; ++i)
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    cout << setprecision(10) << fixed;
+    
+    char a = '1';
+    cout << a;
+    
+    return 0;
+}
