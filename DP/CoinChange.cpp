337afab82ce82c35ca67bae8bd6d67ff8f6ca4da
Marina Rodas <122946394+marsales@users.noreply.github.com>
2026-07-09 15:22:08 -0300

    Add functions to count ways to form K with coins

--- DP/CoinChange.cpp
+++ DP/CoinChange.cpp
@@ -20,3 +20,46 @@ int min_coins_unbounded(const vector<int>& coins, int K) {
 
     return dp[K] == INF ? -1 : dp[K];
 }
+
+
+
+// Conta formas de formar K usando moedas infinitas
+// Ordem importa
+// Complexidade: O(nK)
+
+long long count_ways_unbounded_ordered(const vector<int>& coins, int K) {
+    vector<long long> dp(K + 1, 0);
+
+    dp[0] = 1;
+
+    for (int s = 1; s <= K; s++) {
+        for (int x : coins) {
+            if (s >= x) {
+                dp[s] += dp[s - x];
+            }
+        }
+    }
+
+    return dp[K];
+}
+
+
+
+
+// Conta formas de formar K usando moedas infinitas
+// Ordem não importa
+// Complexidade: O(nK)
+
+long long count_ways_unbounded_orderless(const vector<int>& coins, int K) {
+    vector<long long> dp(K + 1, 0);
+
+    dp[0] = 1;
+
+    for (int x : coins) {
+        for (int s = x; s <= K; s++) {
+            dp[s] += dp[s - x];
+        }
+    }
+
+    return dp[K];
+}
