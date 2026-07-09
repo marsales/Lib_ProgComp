3948bd282eb1049f80b582283d9f9cb2e7e922ab
Marina Rodas <122946394+marsales@users.noreply.github.com>
2026-07-09 15:29:37 -0300

    Improve Subset Sum algorithm with bitset
    Refactored Subset Sum implementation to use bitset for efficiency.

--- DP/SubsetSum.cpp
+++ DP/SubsetSum.cpp
@@ -1,9 +1,7 @@
 // SUBSET SUM
-/*
-Problema: Descobrir se existe um subconjunto de um conjunto de inteiros que some exatamente t
-
-dp[i] -> é possível fazer a soma i?
-*/
+// Descobrir se existe um subconjunto de um conjunto de inteiros que some exatamente t
+// Cada elemento pode ser usado no máximo uma vez
+// dp[i] -> é possível fazer a soma i?
 
 int t;
 vector<int> a;
@@ -18,3 +16,25 @@ for (int x : a) {
 }
 
 cout << dp[t] << endl;
+
+
+
+// SUBSET SUM COM BITSET
+// Descobrir se existe um subconjunto que some exatamente t
+// Cada elemento pode ser usado no máximo uma vez
+// possible[i] = 1 se é possível formar soma i
+// Complexidade prática: O(n * MAXS / 64)
+
+const int MAXS = 200000; // ajuste conforme o limite do problema
+
+int t;
+vector<int> a;
+
+bitset<MAXS + 1> possible;
+possible[0] = 1;
+
+for (int x : a) {
+    possible |= (possible << x);
+}
+
+cout << possible[t] << endl;
