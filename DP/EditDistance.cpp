d867821cd89aeb73ae2b53d3e541767786d481c0
Marina Rodas <122946394+marsales@users.noreply.github.com>
2026-07-10 23:11:39 -0300

    Refactor edit distance to support alignment and LCS
    Refactor edit distance implementation to include string alignment and LCS functionality with customizable scoring.

--- DP/EditDistance.cpp
+++ DP/EditDistance.cpp
@@ -1,32 +1,62 @@
-// EDIT DISTANCE
-// Calcula distância entre duas strings
-// Operações: insert, delete e replace
+// EDIT DISTANCE/STRING ALIGNMENT/LCS
+
+// Edit Distance: menor distância de edição         -> match = 0 | mismatch = insert = remove = 1
+// String Alignment: maior pontuação de alinhamento -> match = 2 | mismatch = insert = remove = -1
+// LCS: longest common subsequence                  -> match = 1 | insert = remove = 0 | mismatch = -INF
+
+// Operações: insert, delete e replace (mismatch)
 
 // Time:   O(|a| * |b|)
 // Memory: O(min(|a|, |b|))
 
-int edit_distance(const string& a, const string& b) {
-    if (a.size() < b.size()) return edit_distance(b, a);
+struct AlignmentScores {
+    int match;
+    int mismatch;
+    int insert;
+    int remove;
+};
 
-    vector<int> dp(b.size() + 1);
-    iota(dp.begin(), dp.end(), 0);
+int string_alignment(
+    const string& a,
+    const string& b,
+    const AlignmentScores& score
+) {
+    int n = (int)a.size();
+    int m = (int)b.size();
 
-    for (int i = 1; i <= (int)a.size(); ++i) {
-        int diagonal = dp[0];
-        dp[0] = i;
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
 
-        for (int j = 1; j <= (int)b.size(); ++j) {
-            int old = dp[j];
+    // Alinhar os primeiros i caracteres de a com a string vazia:
+    // é necessário remover todos eles.
+    for (int i = 1; i <= n; ++i)
+        dp[i][0] = dp[i - 1][0] + score.remove;
 
-            dp[j] = min({
-                dp[j] + 1,                        // remove a[i - 1]
-                dp[j - 1] + 1,                    // insere b[j - 1]
-                diagonal + (a[i - 1] != b[j - 1]) // substitui ou mantém
+    // Alinhar a string vazia com os primeiros j caracteres de b:
+    // é necessário inserir todos eles.
+    for (int j = 1; j <= m; ++j)
+        dp[0][j] = dp[0][j - 1] + score.insert;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            int diagonal =
+                dp[i - 1][j - 1] +
+                (a[i - 1] == b[j - 1]
+                    ? score.match
+                    : score.mismatch);
+
+            int remove_from_a =
+                dp[i - 1][j] + score.remove;
+
+            int insert_in_a =
+                dp[i][j - 1] + score.insert;
+
+            dp[i][j] = max({
+                diagonal,
+                remove_from_a,
+                insert_in_a
             });
-
-            diagonal = old;
         }
     }
 
-    return dp.back();
+    return dp[n][m];
 }
